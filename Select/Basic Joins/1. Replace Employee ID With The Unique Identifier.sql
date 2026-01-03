e6475ecd57d7df092ba723ca0c7994e055381fe6
Ayush Raj <97796452+Ayush1016@users.noreply.github.com>
2026-01-03 11:33:55 +0530

    Create 1. Replace Employee ID With The Unique Identifier.sql

---
+++ Select/Basic Joins/1. Replace Employee ID With The Unique Identifier.sql
@@ -0,0 +1,6 @@
+Problem Link: https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier/description/?envType=study-plan-v2&envId=top-sql-50
+
+select eu.unique_id,e.name
+from Employees as e
+left join EmployeeUNI as eu
+on e.id=eu.id 
