bf0cf755aece5ba0b993c49d27655cc77863c8b0
Ayush Raj <97796452+Ayush1016@users.noreply.github.com>
2026-01-08 23:20:48 +0530

    Create 1. Number of Unique Subjects Taught by Each Teacher.sql

---
+++ Basic Aggregate Functions/1. Number of Unique Subjects Taught by Each Teacher.sql
@@ -0,0 +1,4 @@
+Problem Link: https://leetcode.com/problems/number-of-unique-subjects-taught-by-each-teacher/?envType=study-plan-v2&envId=top-sql-50
+
+Select  teacher_id ,count(distinct subject_id ) cnt
+from Teacher group by teacher_id
